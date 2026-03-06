3d06331b61a8c00333bf500118079bafac5b2eb6
Muaj60 <m.studyboy49@gmail.com>
2026-03-07 01:05:05 +0600

    Another Part complete

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,119 @@
-document.getElementById("login-btn").addEventListener("click",function(){
+const priorityBadge = (priority) => {
+  const p = priority.toUpperCase(); 
+  return `
+    <span class="w-24 text-center px-3 py-1 text-sm rounded-md font-semibold
+      ${
+        p === "HIGH"
+          ? "bg-red-100 text-red-600"
+          : p === "MEDIUM"
+          ? "bg-yellow-100 text-yellow-600"
+          : p === "LOW"
+          ? "bg-gray-100 text-gray-600"
+          : "bg-green-100 text-green-600"
+      }">
+      ${p}
+    </span>
+  `;
+};
 
-    const username=document.getElementById("username").value;
-    const password=document.getElementById("password").value;
 
-    if(username==="admin" && password==="admin123"){
-        alert("Login Successful");
-    }
-    else{
-        alert("Wrong Username or Password");
-    }
+
+const createElement = (labels) => {
+  return labels
+    .map((label) => {
+      if (label.toLowerCase() === "bug") {
+        return `<span class="px-3 py-1 text-xs rounded-full border border-red-400 text-red-500">🐞 BUG</span>`;
+      } 
+      else if (label.toLowerCase() === "help wanted") {
+        return `<span class="px-3 py-1 text-xs rounded-full border border-yellow-400 text-yellow-600">⚙️ HELP WANTED</span>`;
+      } 
+      else if (label.toLowerCase() === "enhancement") {
+        return `<span class="px-3 py-1 text-xs rounded-full border border-green-400 text-green-600">✨ ENHANCEMENT</span>`;
+      } 
+      else if (label.toLowerCase() === "good first issue") {
+        return `<span class="px-3 py-1 text-xs rounded-full border border-blue-400 text-blue-600">🚀 GOOD FIRST ISSUE</span>`;
+      } 
+      else if (label.toLowerCase() === "documentation") {
+        return `<span class="px-3 py-1 text-xs rounded-full border border-gray-400 text-gray-600">📄 DOCUMENTATION</span>`;
+      }
+    })
+    .join(" ");
+};
+
+document.getElementById("login-btn").addEventListener("click", () => {
+
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username === "admin" && password === "admin123") {
+
+    document.getElementById("login-section").classList.add("hidden");
+    document.querySelector("header").classList.remove("hidden");
+    document.getElementById("all-btn").classList.remove("hidden")
+    document.getElementById("count-section").classList.remove("hidden")
+    document.getElementById("issue-section").classList.remove("hidden");
+
+    loadIssue();
+
+  } 
+  else {
+    alert("Wrong Username or Password");
+  }
 
 });
+
+const loadIssue = () => {
+
+  fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    .then((res) => res.json())
+    .then((data) => {
+
+      const issues = data.data;
+      const container = document.getElementById("issue-container");
+
+      container.innerHTML = "";
+
+      issues.forEach((issue) => {
+
+        const div = document.createElement("div");
+
+        div.className = "card bg-white shadow-sm p-5 rounded-xl bg-gray-200";
+
+        div.innerHTML = `
+
+<div class="h-full flex flex-col justify-between mb-2">
+   <div class="flex justify-between">
+   <img class="w-8" src="./assets/Open-Status.png" alt="">
+   ${priorityBadge(issue.priority)}
+   </div>
+  <h2 class="font-semibold text-[18px]">
+    ${issue.title}
+  </h2>
+
+  <p class="text-sm text-gray-500">
+    ${issue.description}
+  </p>
+
+</div>
+
+<div class="flex gap-2 mt-2">
+  ${createElement(issue.labels)}
+</div>
+
+<div class="border-t border-gray-200 mt-2"></div>
+
+<div class="text-sm mt-2 space-y-2 text-gray-400">
+  #${issue.id} by john_doe
+  <br>
+  1/15/2024
+</div>
+
+`;
+
+        container.append(div);
+
+      });
+
+    });
+
+};
